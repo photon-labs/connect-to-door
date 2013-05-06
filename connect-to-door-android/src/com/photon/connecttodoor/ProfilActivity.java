3be6fd000134db07115ade7d1d07e9f446cbf679
Febrianto Samsuri <suryo.prabowo@photoninfotech.net>
2013-05-06 19:22:44 +0700

    initial commit
    commit connect to door android

---
+++ connect-to-door-android/src/com/photon/connecttodoor/ProfilActivity.java
@@ -0,0 +1,17 @@
+package com.photon.connecttodoor;
+
+import com.photon.connecttodoor.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ProfilActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil);
+    }
+
+    
+}
