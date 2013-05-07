6a80b1d004db22bfe866fad980d7841fa1d26c98
Febrianto Samsuri <suryo.prabowo@photoninfotech.net>
2013-05-07 21:01:29 +0700

    fix profil page
    fix ui for profil page

--- connect-to-door-android/src/com/photon/connecttodoor/ProfilActivity.java
+++ connect-to-door-android/src/com/photon/connecttodoor/ProfilActivity.java
@@ -3,15 +3,67 @@ package com.photon.connecttodoor;
 import com.photon.connecttodoor.R;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 import android.app.Activity;
+import android.content.Intent;
 
 public class ProfilActivity extends Activity {
+	
+	private ImageButton attendanceButton,voucherButton,signOutButton; 
+	
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+        attendanceButton = (ImageButton)findViewById(R.id.attendancebutton);
+        voucherButton = (ImageButton)findViewById(R.id.voucherbutton);
+        signOutButton = (ImageButton)findViewById(R.id.signOutButton);
+        
+        attendanceButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				goToAttendancePage();
+				
+			}
+		});
+        
+        voucherButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				goToVoucherPage();
+				
+			}
+		});
+        
+        signOutButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				goToLoginPage();
+				
+			}
+		});
+    }
+    
+    private void goToAttendancePage(){
+    	Intent attendancePage = new Intent(ProfilActivity.this,WelcomeScreenActivity.class);
+    	startActivity(attendancePage);
     }
 
+    private void goToVoucherPage(){
+    	Intent voucherPage = new Intent(ProfilActivity.this,VoucherActivity.class);
+    	startActivity(voucherPage);
+    }
+    
+    private void goToLoginPage(){
+    	Intent loginPage = new Intent(ProfilActivity.this,LoginActivity.class);
+    	startActivity(loginPage);
+    }
+    
     
 }
--- connect-to-door-android/src/com/photon/connecttodoor/VoucherActivity.java
+++ connect-to-door-android/src/com/photon/connecttodoor/VoucherActivity.java
@@ -1,14 +1,46 @@
 package com.photon.connecttodoor;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class VoucherActivity extends Activity {
 	
+	private Button signOutButton,backButton;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_voucher);
+		
+		signOutButton = (Button)findViewById(R.id.signout_button);
+		signOutButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+			goToLoginPage();
+				
+			}
+		});
+		backButton = (Button)findViewById(R.id.back_button);
+		backButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				goToAttandace();
+				
+			}
+		});
 	}
-
+	public void goToLoginPage(){
+		Intent logInPage = new Intent(VoucherActivity.this, LoginActivity.class);
+		startActivity(logInPage);
+	}
+	
+	public void goToAttandace(){
+		Intent attandancePage = new Intent(VoucherActivity.this, WelcomeScreenActivity.class);
+		startActivity(attandancePage);
+	}
 }
