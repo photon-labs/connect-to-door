2e73a788e38ad056ea7ff3d9421e07b36fcce68a
Febrianto Samsuri <suryo.prabowo@photoninfotech.net>
2013-05-13 16:20:11 +0700

    report admin
    make report admin ui

---
+++ connect-to-door-android/src/com/photon/connecttodoor/ReportActivity.java
@@ -0,0 +1,19 @@
+package com.photon.connecttodoor;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.app.Activity;
+import android.content.Intent;
+
+public class ReportActivity extends Activity {
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_report);
+	}
+	
+}
