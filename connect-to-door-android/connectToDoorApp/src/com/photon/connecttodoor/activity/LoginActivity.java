ec59d1a5111a54749d0fd44ccb55f49388b1193a
Fenny Anggraeni <maretska@Photons-Mac-mini-7.local>
2013-07-08 16:24:47 +0700

    change folder and import facebook sdk
    change folder and import facebook sdk

---
+++ connect-to-door-android/connectToDoorApp/src/com/photon/connecttodoor/activity/LoginActivity.java
@@ -0,0 +1,129 @@
+package com.photon.connecttodoor.activity;
+
+import com.photon.connecttodoor.R;
+import com.photon.connecttodoor.utils.Utility;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.app.Activity;
+import android.content.Intent;
+
+import com.facebook.LoggingBehavior;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.Settings;
+import com.facebook.model.GraphUser;
+
+public class LoginActivity extends Activity {
+	private Button facebookButton;
+
+	private Session.StatusCallback statusCallback = new SessionStatusCallback();
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		facebookButton = (Button)findViewById(R.id.signin_button);
+		
+		Settings.addLoggingBehavior(LoggingBehavior.INCLUDE_ACCESS_TOKENS);
+		
+		Session session = Session.getActiveSession();
+        if (session == null) {
+            if (savedInstanceState != null) {
+                session = Session.restoreSession(this, null, statusCallback, savedInstanceState);
+            }
+            if (session == null) {
+                session = new Session(this);
+            }
+            Session.setActiveSession(session);
+            if (session.getState().equals(SessionState.CREATED_TOKEN_LOADED)) {
+                session.openForRead(new Session.OpenRequest(this).setCallback(statusCallback));
+            }
+        }
+
+        updateView();
+	}
+	 @Override
+	    public void onStart() {
+	        super.onStart();
+	        Session.getActiveSession().addCallback(statusCallback);
+	    }
+
+	    @Override
+	    public void onStop() {
+	        super.onStop();
+	        Session.getActiveSession().removeCallback(statusCallback);
+	    }
+
+	    @Override
+	    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	        super.onActivityResult(requestCode, resultCode, data);
+	        Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+	    }
+
+	    @Override
+	    protected void onSaveInstanceState(Bundle outState) {
+	        super.onSaveInstanceState(outState);
+	        Session session = Session.getActiveSession();
+	        Session.saveSession(session, outState);
+	    }
+
+	
+    private void updateView() {
+        final Session session = Session.getActiveSession();
+        if (session.isOpened()) {
+        	 Request request = Request.newMeRequest(session, new Request.GraphUserCallback() {
+
+					@Override
+					public void onCompleted(GraphUser user, Response response) {
+						// TODO Auto-generated method stub
+						if (session == Session.getActiveSession()) {
+	                         if (user != null) {
+	                         	Log.v("RESPONSE", "Response : " + response);
+	                             Log.v("USERID", "UserID : " + user.getId());
+	                             Log.v("FIRSTNAME", "User FirstName : " + user.getFirstName());
+	                             Utility.savePreference("facebookId", user.getId(), getApplicationContext());
+	                             goToSignInPage();
+	                         }   
+	                     }   
+					}   
+             }); 
+             Request.executeBatchAsync(request);
+
+        } else {
+            facebookButton.setOnClickListener(new OnClickListener() {
+                public void onClick(View view) { onClickLogin(); }
+            });
+        }
+    }
+    private void onClickLogin() {
+        Session session = Session.getActiveSession();
+        if (!session.isOpened() && !session.isClosed()) {
+            session.openForRead(new Session.OpenRequest(this).setCallback(statusCallback));
+        } else {
+            Session.openActiveSession(this, true, statusCallback);
+        }
+    }
+
+    public static void onClickLogout() {
+        Session session = Session.getActiveSession();
+        if (!session.isClosed()) {
+            session.closeAndClearTokenInformation();
+        }
+    }
+	public void goToSignInPage(){
+		Intent signInPage = new Intent(LoginActivity.this, SignInActivity.class);
+		startActivity(signInPage);
+	}
+    private class SessionStatusCallback implements Session.StatusCallback {
+        @Override
+        public void call(Session session, SessionState state, Exception exception) {
+            updateView();
+        }
+    }
+
+}
---
+++ connect-to-door-android/connectToDoorApp/src/com/photon/connecttodoor/activity/SignInActivity.java
@@ -0,0 +1,101 @@
+package com.photon.connecttodoor.activity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.photon.connecttodoor.R;
+import com.photon.connecttodoor.controller.LoginService;
+import com.photon.connecttodoor.utils.Utility;
+
+public class SignInActivity extends Activity {
+
+	private Button signInButton;
+	private EditText signInEditText;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sign_in);
+
+		signInEditText = (EditText)findViewById(R.id.edit_message);
+		signInButton = (Button)findViewById(R.id.buttonSignIn);
+		signInButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				int empty = 0;
+				if(signInEditText.getText().length() != empty){
+					new CallServiceLoginTask().execute();
+				}else{
+					alertMessage("Please input your employee id");
+				}
+			}
+		});
+	}		
+	public void goToWelcomePage(){
+		Intent welcomePage = new Intent(SignInActivity.this, WelcomeScreenActivity.class);
+		startActivity(welcomePage);
+	}
+
+	private void alertMessage(String message){
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(SignInActivity.this);
+
+		// set title
+		alertDialogBuilder.setTitle("Alert");
+
+		// set dialog message
+		alertDialogBuilder
+		.setMessage(message)
+		.setCancelable(false)
+		.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog,int id) {
+				// if this button is clicked, close
+				// current activity
+				dialog.dismiss();
+			}
+		});
+
+		// create alert dialog
+		AlertDialog alertDialog = alertDialogBuilder.create();
+
+		// show it
+		alertDialog.show();
+	}
+
+	private class CallServiceLoginTask extends AsyncTask<Void, Void, String> {
+
+		private ProgressDialog dialog;
+
+		protected void onPreExecute() {
+			this.dialog = ProgressDialog.show(SignInActivity.this,
+					"Login Process", "Please Wait...", true);
+		}
+
+		@Override
+		protected String doInBackground(Void... params) {
+			// TODO Auto-generated method stub
+			String employeeId = signInEditText.getText().toString();
+			Utility.savePreference("employeeId", employeeId, getApplicationContext());
+			String fbId = Utility.loadStringPreferences("facebookId", getApplicationContext());
+			LoginService loginService = new LoginService();
+			String response = loginService.handleLoginRequest(employeeId, fbId);
+			return response;
+		}
+
+		protected void onPostExecute(String result) {
+			Utility.savePreference("responseLogin", result, getApplicationContext());
+			goToWelcomePage();
+			this.dialog.dismiss();
+		}
+	}
+
+}
