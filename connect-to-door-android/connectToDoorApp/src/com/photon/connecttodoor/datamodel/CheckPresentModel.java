6ae976d7d4dcd3ac0d79338fcc18b591fa86ae85
Febrianto Samsuri <febrianto.samsuri@photoninfotech.net>
2013-07-10 19:08:21 +0700

    add check present model class

--- connect-to-door-android/connectToDoorApp/src/com/photon/connecttodoor/datamodel/CheckPresentModel.java
+++ connect-to-door-android/connectToDoorApp/src/com/photon/connecttodoor/datamodel/CheckPresentModel.java
@@ -4,21 +4,25 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class CheckPresentModel {
-	String checkout;
-	String checkin;
+	String checkoutPresent;
+	String checkinPresent;
 	String presenceId;
 	String employeeId;
-	public String getCheckout() {
-		return checkout;
+	String message;
+	String status;
+	String check_in;
+	String check_out;
+	public String getCheckoutPresent() {
+		return checkoutPresent;
 	}
-	public void setCheckout(String checkout) {
-		this.checkout = checkout;
+	public void setCheckoutPresent(String checkoutPresent) {
+		this.checkoutPresent = checkoutPresent;
 	}
-	public String getCheckin() {
-		return checkin;
+	public String getCheckinPresent() {
+		return checkinPresent;
 	}
-	public void setCheckin(String checkin) {
-		this.checkin = checkin;
+	public void setCheckinPresent(String checkinPresent) {
+		this.checkinPresent = checkinPresent;
 	}
 	public String getPresenceId() {
 		return presenceId;
@@ -32,20 +36,56 @@ public class CheckPresentModel {
 	public void setEmployeeId(String employeeId) {
 		this.employeeId = employeeId;
 	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getCheck_in() {
+		return check_in;
+	}
+	public void setCheck_in(String check_in) {
+		this.check_in = check_in;
+	}
+	public String getCheck_out() {
+		return check_out;
+	}
+	public void setCheck_out(String check_out) {
+		this.check_out = check_out;
+	}
 	
 	public void parseSource(String response) throws JSONException {
 		JSONObject responseObject = new JSONObject(response);
 		if(responseObject.has("check_out")){
-			this.setCheckout(responseObject.getString("check_out"));
+			this.setCheckoutPresent(responseObject.getString("check_out"));
 		}
 		if(responseObject.has("check_in")){
-			this.setCheckin(responseObject.getString("check_in"));
+			this.setCheckinPresent(responseObject.getString("check_in"));
 		}
 		if(responseObject.has("presence_id")){
 			this.setPresenceId(responseObject.getString("presence_id"));
 		}
 		if(responseObject.has("employee_id")){
 			this.setEmployeeId(responseObject.getString("employee_id"));
+		}
+		if(responseObject.has("message")){
+			this.setMessage(responseObject.getString("message"));
+		}
+		if(responseObject.has("status")){
+			this.setStatus(responseObject.getString("status"));
+		}
+		if(responseObject.has("checkOut")){
+			this.setCheck_out(responseObject.getString("checkOut"));
+		}
+		if(responseObject.has("checkIn")){
+			this.setCheck_in(responseObject.getString("checkIn"));
 		}
 	}
 }
