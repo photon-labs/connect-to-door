66818ea154d3fd0c1edef315617af56363d06911
GagahHidayat <gagah.hidayat@photoninfotech.net>
2013-07-24 15:13:59 +0700

    add resources for attendance list module

--- connect-to-door-ios/ConnectToDoor/Classes/AttendanceList/ViewController/ctdAttendanceListViewController.h
+++ connect-to-door-ios/ConnectToDoor/Classes/AttendanceList/ViewController/ctdAttendanceListViewController.h
@@ -9,10 +9,28 @@
 #import <UIKit/UIKit.h>
 #import "ctdBaseViewController.h"
 #import "ctdAttendanceListService.h"
+#import "DropDownView.h"
 
-@interface ctdAttendanceListViewController : ctdBaseViewController <AttendanceListServiceDelegate>{
-    IBOutlet UITableView *attendanceTableView;
+@interface ctdAttendanceListViewController : ctdBaseViewController <AttendanceListServiceDelegate, DropDownViewDelegate> {
+    
+	DropDownView *dropDownView;
+	IBOutlet UITableView *attendanceTableView;
     NSMutableArray *attendanceContentArray;
 }
+
+@property (nonatomic, strong) IBOutlet UILabel *searchOptionLabel;
+@property (nonatomic, strong) IBOutlet UITextField *dateStartText;
+@property (nonatomic, strong) IBOutlet UITextField *dateEndText;
+@property (nonatomic, strong) IBOutlet UITextField *searchKeyText;
+@property (nonatomic, strong) IBOutlet UIButton *searchOptionButton;
+@property (nonatomic, strong) IBOutlet UIButton *dateStartButton;
+@property (nonatomic, strong) IBOutlet UIButton *dateEndButton;
+@property (nonatomic, strong) IBOutlet UIButton *searchButton;
+
+- (IBAction)didDateStartClicked:(id)sender;
+- (IBAction)didDateEndClicked:(id)sender;
+- (IBAction)didSearchOptionClicked:(id)sender;
+- (IBAction)didSearchClicked:(id)sender;
+
 
 @end
