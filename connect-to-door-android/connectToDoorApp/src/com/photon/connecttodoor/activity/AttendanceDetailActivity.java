231262adc9c203f2ffd2928eba54e490c93f6a39
fadli <maretska2@PTs-Mac-mini-2.local>
2013-07-30 11:32:35 +0700

    implement click list when user tap attendance list
    implement click list when user tap attendance list

--- connect-to-door-android/connectToDoorApp/src/com/photon/connecttodoor/activity/AttendanceDetailActivity.java
+++ connect-to-door-android/connectToDoorApp/src/com/photon/connecttodoor/activity/AttendanceDetailActivity.java
@@ -1,15 +1,70 @@
 package com.photon.connecttodoor.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 import com.photon.connecttodoor.R;
+import com.photon.connecttodoor.utils.ApplicationConstant;
 
 public class AttendanceDetailActivity extends MainActivity {
 
+	Button backButton;
+	Button signOutButton;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_attendance_detail);
 
-	}	
+		backButton = (Button)findViewById(R.id.back_buttonattdetail);
+		signOutButton = (Button)findViewById(R.id.signout_buttonattdetail);
+		this.actionButton();
+	}
+	private void actionButton(){
+		/**
+		 * onClick back button
+		 */
+		backButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				goToAttendanceListPage();
+			}
+		});
+		/**
+		 * onClick sign out button
+		 */
+		signOutButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				/**check internet connection before sign out application */
+				if(connectionAvailable()){
+					LoginActivity.onClickLogout();
+					goToLogin();
+				}else{
+					alertMessage(ApplicationConstant.NO_INTERNET_CONNECTION, AttendanceDetailActivity.this);
+				}
+			}
+		});
+
+	}
+	/**
+	 * launch attendance list page
+	 */
+	private void goToAttendanceListPage(){
+		finish();
+	}
+	/**
+	 * launch login page
+	 */
+	private void goToLogin(){
+		Intent intentLoginPage = new Intent(AttendanceDetailActivity.this, LoginActivity.class);
+		startActivity(intentLoginPage);
+		finish();
+	}
+
 }
